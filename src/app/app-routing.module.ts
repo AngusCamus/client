f80f3d60cc2251ab4a2ab3a289eff21a241a7f36
Brian Polesel <brianpolesel07@gmail.com>
2022-12-02 21:42:29 -0300

    add structure for client angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import {PostListComponent} from './components/post-list/post-list.component'
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PostListComponent
+  },
+  {
+    path: 'posts',
+    component: PostListComponent
+  },
+  {
+    path: 'post/create',
+    component: PostFormComponent
+  },
+  {
+    path: 'post/edit/:id',
+    component: PostFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostListComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
